4206595da04162c2b3543344728be825fc2bc009
azarovalex <alex@azarov.by>
2018-03-01 23:23:26 +0300

    Solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let longest_streak = 0
+  let num_set = new Set(array)
+
+
+  for (let num of num_set) {
+    if (num_set.has(num - 1) == false) {
+      let current_num = num;
+      let current_streak = 1;
+
+      while (num_set.has(current_num + 1)) {
+        current_num += 1;
+        current_streak += 1;
+      }
+
+      longest_streak = Math.max(current_streak, longest_streak);
+    }
+  }
+
+ return longest_streak
 }
